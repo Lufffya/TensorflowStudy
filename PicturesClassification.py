eb5f8542f0674a60358d61fc96d69c7a7c9474eb
LuFei <571036709@qq.com>
2020-09-02 11:32:03 +0800

    commpany commit

--- PicturesClassification.py
+++ PicturesClassification.py
@@ -78,7 +78,7 @@ model = tf.keras.Sequential()
 model.add(tf.keras.layers.Conv2D(32,(3,3),activation="relu",input_shape=(150,150,3)))
 model.add(tf.keras.layers.MaxPooling2D(2,2))
 
-model.add(tf.keras.layers.Conv2D(64,(3,3),activation="relu"))
+model.add(tf.keras.layers.Conv2D(32,(3,3),activation="relu"))
 model.add(tf.keras.layers.MaxPooling2D((2,2)))
 
 model.add(tf.keras.layers.Conv2D(64,(3,3),activation="relu"))
