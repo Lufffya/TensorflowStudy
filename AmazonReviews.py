87e168fa24d80058c1ff5c8906e2d22933f87e45
LuFei <571036709@qq.com>
2020-09-15 09:42:25 +0800

    company commit

--- AmazonReviews.py
+++ AmazonReviews.py
@@ -1,3 +1,4 @@
+import os
 import bert
 import numpy as np
 import tensorflow as tf
@@ -12,18 +13,15 @@ from matplotlib import pyplot
 '''读取模型'''
 
 bert_params = bert.params_from_pretrained_ckpt("Models\\Pretraining_Bert_EN_Uncased_L-12_H-768_A-12_Google-Research")
-
 bert_layer = bert.BertModelLayer.from_params(bert_params, name="bert")
-
 tokenizer = bert.bert_tokenization.FullTokenizer(vocab_file="Models\\Bert_EN_Uncased_L-12_H-768_A-12_Google-Research\\vocab.txt")
 
 
 '''语义提取'''
-max_seq_length = 128
-
+max_seq_length = 32
 train_data = pd.read_csv("DataSet\AmazonReviews(B07PBV7D48).csv")
-
 print(len(train_data))
+
 
 train_X = []
 for review in train_data.values:
@@ -41,9 +39,23 @@ def Get_Encoding(inputs):
     pooling = tf.keras.layers.GlobalAveragePooling1D()
     for i in tqdm(range(0,len(inputs),32)):
         batch = inputs[i:i+32]
-        batch_word_embedding = pooling(bert_layer(batch))
-        outPut.extend(batch_word_embedding.numpy())
+        # batch_word_embedding = pooling(bert_layer(batch))
+        batch_word_embedding = bert_layer(batch)
+
+        #batch * 128 * 768
+        for _batch in batch_word_embedding:
+            #1 * 128 * 768
+            array = []
+            for j in range(768):
+                #128 * 1
+                temp = 0
+                for k in range(len(_batch)):
+                    temp += _batch[k][j]
+                array.append(temp/(len(_batch)))            
+            outPut.append(array)
+        # outPut.extend(batch_word_embedding.numpy())
     return np.array(outPut)
+
 
 # 解析标签
 target = ["very easy to use"]
@@ -67,57 +79,76 @@ _zip = zip(range(len(euclidean_Distance)),euclidean_Distance)
 
 _sorted = sorted(_zip,key=lambda x: x[1],reverse=False)
 
-for _index, distance in _sorted[0:20]:
+for _index, distance in _sorted[0:50]:
     print(train_data.values[_index], "(Distance: %.4f)" % (distance))
 
 
 
-'''文本聚类'''
-clusterData =  train_X_Encoding
+# '''文本聚类'''
+# clusterData =  train_X_Encoding
 
-# 初始化聚类函数
-kMeans_Model = KMeans(n_clusters=20,max_iter=500,tol=0.0001,verbose=True,n_jobs=10)
+# # 初始化聚类函数
+# kMeans_Model = KMeans(n_clusters=20,max_iter=5000,tol=0.00001,verbose=True,n_jobs=10)
 
-# 训练或者叫做执行聚类操作
-kMeans_Model.fit(clusterData)
+# # 训练或者叫做执行聚类操作
+# kMeans_Model.fit(clusterData)
 
-# K组数据点的每个中心点
-centers = kMeans_Model.cluster_centers_
+# # K组数据点的每个中心点
+# centers = kMeans_Model.cluster_centers_
 
-# 每个数据点所属分组
-labels = kMeans_Model.labels_
+# # 每个数据点所属分组
+# labels = kMeans_Model.labels_
 
-print(list(set(labels)))
-
-firstClassIndex = []
-
-for index,item in enumerate(labels):
-    if item == 0 :
-        firstClassIndex.append(index)
+# print(list(set(labels)))
 
 
-firstClassResult = []
+# import csv
 
-for index in firstClassIndex:
-    firstClassResult.append(train_data.values[index])
+# _class = list(set(labels))
+
+# for _label in _class:
+#     classIndex = []
+#     for _index,_item in enumerate(labels):
+#         if _item == _label :
+#             classIndex.append(_index)
+
+#     classResult = []
+#     for index in classIndex:
+#         classResult.append(train_data.values[index])
+    
+#     csv_File = open('DataSet\新建文本文档{0}.csv'.format(_label),'w',encoding='utf-8')
+#     csv_writer = csv.writer(csv_File)
+#     for review in classResult:
+#         csv_writer.writerow(review)
 
 
-import csv
-
-csv_File = open('DataSet\新建文本文档.csv','w',encoding='utf-8')
-
-csv_writer = csv.writer(csv_File)
-
-for review in firstClassResult:
-    csv_writer.writerow(review)
-
-r1 = pd.Series(kMeans_Model.labels_).value_counts() #统计各个类别的数目
 
 
-r2 = pd.DataFrame(kMeans_Model.cluster_centers_) #找出聚类中心
+# firstClassIndex = []
+
+# for index,item in enumerate(labels):
+#     if item == 0 :
+#         firstClassIndex.append(index)
+
+
+# firstClassResult = []
+
+# for index in firstClassIndex:
+#     firstClassResult.append(train_data.values[index])
+
+# import csv
+
+# csv_File = open('DataSet\新建文本文档.csv','w',encoding='utf-8')
+# csv_writer = csv.writer(csv_File)
+
+# for review in firstClassResult:
+#     csv_writer.writerow(review)
+
+
+#r1 = pd.Series(kMeans_Model.labels_).value_counts() #统计各个类别的数目
+
+#r2 = pd.DataFrame(kMeans_Model.cluster_centers_) #找出聚类中心
  
-
-
 
 # for i in range(len(labels)):
 #     pyplot.scatter(x[i][0], x[i][1], c=('r' if labels[i] == 0 else 'b'))
@@ -132,7 +163,6 @@ r2 = pd.DataFrame(kMeans_Model.cluster_centers_) #找出聚类中心
 #     pyplot.scatter(predict[i][0], predict[i][1], c=('r' if label[i] == 0 else 'b'), marker='x')
  
 # pyplot.show()
-
 
 
 print()
