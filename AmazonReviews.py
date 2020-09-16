83887575a3fac40946b258f958066d12714b36cf
赵新晨 <571036709@qq.com>
2020-09-16 17:42:52 +0800

    company commit

--- AmazonReviews.py
+++ AmazonReviews.py
@@ -2,41 +2,51 @@
 # 亚马逊评论数据文本聚类
 #
 
-import xlwt
 import bert
 import numpy as np
+import tensorflow as tf
 import pandas as pd
+import tensorflow_hub as hub
 import scipy.spatial
 from tqdm import tqdm
-import tensorflow as tf
 from sklearn.cluster import KMeans
+import xlwt
 
-
-#加载Bert模型
+#'''读取模型'''
 bert_params = bert.params_from_pretrained_ckpt("Models\\Pretraining_Bert_EN_Uncased_L-12_H-768_A-12_Google-Research")
 bert_layer = bert.BertModelLayer.from_params(bert_params, name="bert")
 tokenizer = bert.bert_tokenization.FullTokenizer(vocab_file="Models\\Bert_EN_Uncased_L-12_H-768_A-12_Google-Research\\vocab.txt")
-bert_Model = tf.keras.Sequential([bert_layer,tf.keras.layers.GlobalAveragePooling1D()])
+
+#'''语义提取'''
+max_seq_length = 32
+# 过滤句子长度
+train_data = []
+for item in pd.read_csv("DataSet\\AmazonReviews(B07PBV7D48).csv").values :
+    tokens = tokenizer.tokenize(item[0])
+    token_ids = tokenizer.convert_tokens_to_ids(tokens)
+    if len(token_ids) > max_seq_length:
+        continue 
+    train_data.append(item)
+train_data = np.array(train_data)
+
+print(len(train_data))
 
 # 获取所有句子的词索引
-# max_seq_length = 128
-train_data = pd.read_csv("DataSet\AmazonReviews(B07PBV7D48).csv")
-print(len(train_data))
 train_X = []
-for review in train_data.values:
+for review in train_data:
     tokens = tokenizer.tokenize(review[0])
     token_ids = tokenizer.convert_tokens_to_ids(tokens)
-    if len(token_ids) > 32 : 
-        continue
     train_X.append(np.array(token_ids))
 train_X = np.array(train_X)
+
 print(len(train_X))
 
 # 根据句索引获取句向量
 def Get_Encoding(inputs):
     outPut = []
+    model = tf.keras.Sequential([bert_layer,tf.keras.layers.GlobalAveragePooling1D()])
     for item in tqdm(inputs):
-        word_embedding = bert_Model.predict(item)
+        word_embedding = model.predict(item)
         # 对每个维度的每个句子长度做平均处理
         itemTensor  = []
         for i in range(768):
@@ -46,6 +56,15 @@ def Get_Encoding(inputs):
             itemTensor.append(tensor/(len(item)))      
         outPut.append(np.array(itemTensor))
     return np.array(outPut)
+
+# 解析标签
+# target = ["very easy to use"]
+# target_Token = tokenizer.tokenize(target[0])
+# target_ids = tokenizer.convert_tokens_to_ids(target_Token)
+# target_ids = np.array([np.array(target_ids)])
+
+# 获取标签句子的词向量
+# target_X_Encoding = Get_Encoding(target_ids)
 
 # 获所有训练句子的词向量
 train_X_Encoding = Get_Encoding(train_X)
@@ -85,7 +104,6 @@ labels = kMeans_Model.labels_
 
 print(list(set(labels)))
 
-
 # 将聚类结果写入Execl
 workbook = xlwt.Workbook(encoding='utf-8')
 
@@ -99,13 +117,13 @@ for _label in _class:
 
     classResult = []
     for index in classIndex:
-        classResult.append(train_data.values[index])
+        classResult.append(train_data[index])
 
     sheet = workbook.add_sheet("聚类{0}".format(_label))
     for row in range(0, len(classResult)):
         sheet.write(row,0,str(classResult[row][0]))
     
-workbook.save(r'DataSet\\test.xlsx')
+workbook.save(r'DataSet\\KMaensResult.xlsx')
 
 #r1 = pd.Series(kMeans_Model.labels_).value_counts() #统计各个类别的数目
 #r2 = pd.DataFrame(kMeans_Model.cluster_centers_) #找出聚类中心
@@ -123,5 +141,6 @@ workbook.save(r'DataSet\\test.xlsx')
 #     pyplot.scatter(predict[i][0], predict[i][1], c=('r' if label[i] == 0 else 'b'), marker='x')
  
 # pyplot.show()
+
 
 print()
